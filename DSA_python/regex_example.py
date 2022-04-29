1dba111bce3d9d01970a10f870c9252e2ddd4205
Ankit <97289540+Studyankit@users.noreply.github.com>
2022-04-29 20:50:36 +0530

    [Ankit]-Updated regex solution

--- DSA_python/regex_example.py
+++ DSA_python/regex_example.py
@@ -1,10 +1,9 @@
-import re       # Importing regex library
+import re
 
 
-def replace(string):  # Function to replace elements in the string
-
-    print("--------------------------------------------------------------------------")
-    print("After~~~~>>")       # Decorator message
+def regex_example(string):  # Function to replace elements in the string
+    
+    print("After~~~~>>")
 
     print("Replacing User Name at index: ", string.index('<<name>>'))
     string = (re.sub(r"<<name>>", "Ank", string))
@@ -16,8 +15,7 @@ def replace(string):  # Function to replace elements in the string
     string = (re.sub(r"xxxxxxxxxx", "1234567890", string))
 
     print("Replacing Date at index: ", string.index('01/01/2016'))
-    string = (re.sub(r"01/01/2016", "12/02/2022", string))
-    print("--------------------------------------------------------------------------")
+    string = (re.sub(r"01/01/2016", "29/04/2022", string))
 
     return string
 
@@ -27,15 +25,6 @@ string = """Hello <<name>>, We have your full name as <<full name>> in our syste
 Your contact number is 91-xxxxxxxxxx. Please,let us know in case of any clarification. 
 Thank you BridgeLabz. 01/01/2016"""
 
-print("\n")
-print("--------------------------------------------------------------------------")
-print("Before Replacing any elements----------------->>")
-print("\n")
 print(string)
-print("--------------------------------------------------------------------------")
 
-print(replace(string))      # Function Calling
-print("--------------------------------------------------------------------------")
-
-
-print("---------End of program--------")
+print(regex_example(string))
