875ea5c8f3e316ab8202a31941b2f3d349177ef9
StudyAnkit <ankitghosh2018@gmail.com>
2022-05-04 08:43:01 +0530

    [Ankit]-Data structure problems added

---
+++ Data_Structure/Banking_Cash_Counter.py
@@ -0,0 +1,29 @@
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
+        print("Welcome to the Deposit & Withdrawal Machine")
+
+    def deposit(self):
+        amount = float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:", amount)
+
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance >= amount:
+            self.balance -= amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+
+    def display(self):
+        print("\n Net Available Balance=", self.balance)
+
+
+if __name__ == "__main__":
+
+    s = BankAccount()
+
+    s.deposit()
+    s.withdraw()
+    s.display()
